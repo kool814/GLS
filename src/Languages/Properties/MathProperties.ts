ae14d6059af489188c57a25caa41bf3a9552ab4b
chris-j-tang <chrisjtang98@gmail.com>
2017-03-04 11:55:45 -0500

    Removed trailing whitespace from MathProperties.ts

--- src/Languages/Properties/MathProperties.ts
+++ src/Languages/Properties/MathProperties.ts
@@ -18,6 +18,7 @@ export class MathProperties {
      * Any imports native math commands require.
      */
     public requiredImports: { [i: string]: string[] };
+
     /**
      * The name of the math global or namespace.
      */
