45a2fa61d7141310ff872acab8df6a7e6a96bfc4
chris-j-tang <chrisjtang98@gmail.com>
2017-05-13 10:46:21 -0400

    Added test for when bannedKeywordsBag returns true

---
+++ test/unit/Commands/BannedKeywordsBagTests.ts
@@ -0,0 +1,22 @@
+import { expect } from "chai";
+import "mocha";
+
+import { BannedKeywordsBag } from "../../../lib/Commands/BannedKeywordsBag";
+import { LiteralCommand } from "../../../lib/Commands/LiteralCommand";
+import { ConversionContext } from "../../../lib/Conversions/ConversionContext";
+import { TypeScript } from "../../../lib/Languages/TypeScript";
+
+describe("BannedKeywordsBag", () => {
+    describe("renderBannedKeyword", () => {
+        it("returns true for a banned keyword", () => {
+            // Arrange
+            const bannedKeywordsBag = new BannedKeywordsBag();
+
+            // Act
+            const keyword = bannedKeywordsBag.validateName("assert");
+
+            // Assert
+            expect(keyword).to.equal("true");
+        });
+    });
+});
