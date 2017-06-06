b189664a7e8211d0a7028cfc04186c03a2ed9c0b
kool814 <karthik.s814@gmail.com>
2017-06-06 15:45:34 -0700

    Fixes to the PR review

--- src/Languages/Properties/ListProperties.ts
+++ src/Languages/Properties/ListProperties.ts
@@ -13,10 +13,12 @@ export class ListProperties {
      * The name of the list class.
      */
     public className: string;
+
     /**
      * Adds two or more lists together
      */
      public concat: NativeCallProperties;
+     
     /**
     /**
      * How to retrieve the length of a list.
